aeb86e0a54915f1f7e654b4220e4661900c2a200
jandebr <34720253+jandebr@users.noreply.github.com>
2020-01-16 22:36:49 +0100

    Added darkness depth function parameter and scene as context argument to some methods

--- src/org/maia/cgi/model/d3/scene/SceneBuilder.java
+++ src/org/maia/cgi/model/d3/scene/SceneBuilder.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 import org.maia.cgi.compose.Compositing;
 import org.maia.cgi.compose.d3.DepthBlurParameters;
+import org.maia.cgi.compose.d3.DepthFunction;
 import org.maia.cgi.geometry.d3.Point3D;
 import org.maia.cgi.gui.d3.renderer.RenderOptions;
 import org.maia.cgi.model.d3.camera.Camera;
@@ -25,16 +26,20 @@ public abstract class SceneBuilder {
 	}
 
 	public Scene build(RenderOptions options) {
-		Scene scene = new Scene(getSceneName(), createCamera(options));
+		Scene scene = createEmptyScene(options);
 		for (Object3D object : createTopLevelObjects(options)) {
 			scene.addTopLevelObject(object);
 		}
-		for (LightSource light : createLightSources(options)) {
+		for (LightSource light : createLightSources(scene, options)) {
 			scene.addLightSource(light);
 		}
-		scene.setBackdrop(createBackdrop(options));
-		initRenderParameters(scene.getRenderParameters(), options);
+		scene.setBackdrop(createBackdrop(scene, options));
+		initRenderParameters(scene, options);
 		return scene;
+	}
+
+	protected Scene createEmptyScene(RenderOptions options) {
+		return new Scene(getSceneName(), createCamera(options));
 	}
 
 	protected abstract String getSceneName();
@@ -43,20 +48,26 @@ public abstract class SceneBuilder {
 
 	protected abstract Collection<Object3D> createTopLevelObjects(RenderOptions options);
 
-	protected abstract Collection<LightSource> createLightSources(RenderOptions options);
+	protected abstract Collection<LightSource> createLightSources(Scene scene, RenderOptions options);
 
-	protected ColorDepthBuffer createBackdrop(RenderOptions options) {
+	protected ColorDepthBuffer createBackdrop(Scene scene, RenderOptions options) {
 		return null;
 	}
 
-	protected void initRenderParameters(SceneRenderParameters parameters, RenderOptions options) {
+	protected void initRenderParameters(Scene scene, RenderOptions options) {
+		SceneRenderParameters parameters = scene.getRenderParameters();
 		parameters.setAmbientColor(Compositing.setTransparency(options.getSceneBackgroundColor(), 1.0));
 		parameters.setShadowsEnabled(options.isShadowsEnabled());
 		parameters.setBackdropEnabled(options.isBackdropEnabled());
-		parameters.setDepthBlurParameters(createDepthBlurParameters(options));
+		parameters.setDarknessDepthFunction(createDarknessDepthFunction(scene, options));
+		parameters.setDepthBlurParameters(createDepthBlurParameters(scene, options));
 	}
 
-	protected DepthBlurParameters createDepthBlurParameters(RenderOptions options) {
+	protected DepthFunction createDarknessDepthFunction(Scene scene, RenderOptions options) {
+		return null;
+	}
+
+	protected DepthBlurParameters createDepthBlurParameters(Scene scene, RenderOptions options) {
 		return new DepthBlurParameters(0.6, 1.0);
 	}
 
